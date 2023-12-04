828c0ea1bef338a5d94f24f725a9017b373f2ecc
madarapower <mister.igor33@mailru>
2023-12-04 23:36:36 +0300

    s

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+
+export const Navbar = () => {
+  return (
+    <div className='Container'>
+    <div className='Wrapper'>
+        <div className='Left'>left</div>
+        <div className='Center'>zdarova</div>
+        <div className='Right'>bom</div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Navbar;
